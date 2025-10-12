9cf75e31fe19ab59acec27fc5cc9f1b6da821958
ascalise <calabrone74@gmail.com>
2025-10-12 18:10:36 +0200

    Update Sum-med.ts

--- sum-med.ts
+++ sum-med.ts
@@ -1,14 +1,12 @@
 let array: number[] = [1, 2, 3, 4, 5];
 let somma: number = 0;
 let media: number = 0;
-let count: number = 0;
 
 for (let x of array) {
-    count += 1;
     somma = somma + x;
 }
 
-media = somma / count;
+media = somma / array.length;
 
 console.log('Somma =', somma);
 console.log('Media = ', media);
